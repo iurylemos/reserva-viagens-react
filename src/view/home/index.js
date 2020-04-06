a315a7be2c397a4904c73ed7f5325a54e3a875d0
Iury Lemos <iurylemos10@gmail.com>
2020-04-06 02:26:16 -0300

    Criando a home e estilizando

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import api from '../../services/api';
+import { MdFlightTakeoff } from 'react-icons/md'
+import './home.css';
 
 export default function Home() {
 
@@ -19,8 +21,24 @@ export default function Home() {
 
 
   return (
-    <h1>
-      HOME
-    </h1>
+    <div>
+      <div className="box">
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <img src={trip.image} alt="imagem" />
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? 'Disponível' : 'Indisponível'}</span>
+            <button type="button" onClick={() => { }}>
+              <div>
+                <MdFlightTakeoff size={16} color="#FFF" />
+              </div>
+              <span>
+                SOLICITAR RESERVA
+              </span>
+            </button>
+          </li>
+        ))}
+      </div>
+    </div>
   )
 }
