10200f3eb2bcba81cfaf597570b80954fe114d35
Iury Lemos <iurylemos10@gmail.com>
2020-04-06 14:59:26 -0300

    Criando a estilização das Reserva

--- src/view/reservas/index.js
+++ src/view/reservas/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './reservas.css';
+import { MdDelete } from 'react-icons/md';
 
 export default function Reservas() {
   return (
     <div>
-      Reservas
+      <h1 className="title">
+        Você solicitou: 1 Reservas
+      </h1>
+      <div className="reservas">
+        <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg" alt="maceio" />
+        <strong>Viagem Maceio 7 dias</strong>
+        <span>Quantidade: 2</span>
+        <button type='button' onClick={() => { }}>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+      <footer>
+        <button type="button">Solicitar reservas</button>
+      </footer>
     </div>
   )
 }
