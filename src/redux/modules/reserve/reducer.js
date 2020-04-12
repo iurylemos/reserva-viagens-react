a204e416832b027cc3b04e2801fcf2cf3f9597d7
Iury Lemos <iurylemos10@gmail.com>
2020-04-12 12:44:34 -0300

    Adicionando quantidade e utilizando o immer

--- src/redux/modules/reserve/reducer.js
+++ src/redux/modules/reserve/reducer.js
@@ -1,15 +1,40 @@
+import produce from 'immer';
+
 export default function reserve(state = [], action) {
-  console.log(state)
+  // console.log(state)
   // console.log(action)
   //Caso tenha o ação de adicionar uma reserva ele faz uma ação
   //Que vai ser adicionar no array que é o state
   //Caso não ele retorna o state como padrão
+
+  //como o state ele é imutável e não posso dar um push diretamente
+  //Em objetos iguais, vou instalar o immer
+  //E com o immer posso utilizar todas as funções de javascript
+  //Sem se importar com a parte de imutabilidade
+
   switch (action.type) {
     case 'ADD_RESERVE':
       // Estou pegando tudo que tem no array com o ...state
       //E adicionando o conteudo com o action.trip
+      //draft é uma copia do state
 
-      return [...state, action.trip];
+      return produce(state, drafState => {
+        //Verificar se já existe algum state igual ao que estou adicionando
+        //Faço isso usando o find index que é um objeto
+        const tripIndex = drafState.findIndex(trip => trip.id === action.trip.id)
+
+        //Se já existir ele só adicionar mais um na quantidade
+        //Se não ele vai adicionar todo o trip
+        if (tripIndex >= 0) {
+          drafState[tripIndex].quantidade += 1
+        } else {
+          drafState.push({
+            ...action.trip,
+            quantidade: 1
+          })
+        }
+
+      })
     default:
       return state;
   }
--- src/view/reservas/index.js
+++ src/view/reservas/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './reservas.css';
 import { MdDelete } from 'react-icons/md';
 
 export default function Reservas() {
+
+  const reserves = useSelector(state => state.reserve)
+
+  console.log('MINHAS RESERVAS', reserves)
+
   return (
     <div>
       <h1 className="title">
-        Você solicitou: 1 Reservas
+        Você solicitou: {reserves.length} Reservas
       </h1>
-      <div className="reservas">
-        <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg" alt="maceio" />
-        <strong>Viagem Maceio 7 dias</strong>
-        <span>Quantidade: 2</span>
-        <button type='button' onClick={() => { }}>
-          <MdDelete size={20} color="#191919" />
-        </button>
-      </div>
+      {reserves.map(reserve => (
+        <div key={reserve.id} className="reservas">
+          <img src={reserve.image} alt={reserve.title} />
+          <strong>{reserve.title}</strong>
+          <span>Quantidade: {reserve.quantidade}</span>
+          <button type='button' onClick={() => { }}>
+            <MdDelete size={20} color="#191919" />
+          </button>
+        </div>
+      ))}
+
       <footer>
         <button type="button">Solicitar reservas</button>
       </footer>
